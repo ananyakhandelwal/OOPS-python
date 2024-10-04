65df30dd9461bf42e6de99b9d30f0d61f4b100e1
ananyakhandelwal <ananyaakkhandelwal562@gmail.com>
2024-10-04 20:06:55 +0530

    Create only_digits.py

---
+++ Lab-4/only_digits.py
@@ -0,0 +1,7 @@
+str = input("Enter a string: ")
+
+# Check if the string contains only digits
+if str.isdigit():
+    print("The string contains only digits.")
+else:
+    print("The string contains non-digit characters.")
